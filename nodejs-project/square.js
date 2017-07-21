124d62fb903c2c2aa1ef5a9a57d5e9f286db4a8e
ZaninAndrea <99.zanin@gmail.com>
2017-07-21 19:15:18 +0200

    refactoring square

--- nodejs-project/square.js
+++ nodejs-project/square.js
@@ -1,7 +1,3 @@
-function square (x){
-  return Math.pow(x,2)
-}
-
 var numbers = [1,2,3,4,5,6,10,20]
-var squares = numbers.map(square)
+var squares = numbers.map((x) => Math.pow(x,2)) // notice the lambda function :)
 console.log(squares)
